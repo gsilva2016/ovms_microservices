bddd50259c4534983870b8fefbab949a742e12bd
gabriel-intel <gabriel.l.silva@intel.com>
2023-05-01 15:50:50 -0700

    Add display id

--- object_detection/docker-run.sh
+++ object_detection/docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xhost +
-docker run -it --user root --net host --entrypoint /bin/bash -v `pwd`:/savedir -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --privileged ovms_capi_ocv_gst-people:latest
+docker run -it --user root --net host --entrypoint /bin/bash -v `pwd`:/savedir  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --privileged ovms_capi_ocv_gst-people:latest
