6b44371eecdd4b3790adb97486e97ac333dc8989
gsilva2016 <gabriel.l.silva@intel.com>
2023-05-01 19:23:05 -0700

    bugfix: resolved build issues

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ make
 cd ..
 
 echo "Downloading sample video coca-cola-4465029.mp4 https://www.pexels.com/video/4465029/download/"
-cd yolov5
+cd object_detection
 wget -O coca-cola-4465029.mp4 https://www.pexels.com/video/4465029/download/
 wget -O people-detection.mp4 https://github.com/gsilva2016/sample-videos/raw/12_fps/Store-Aisle-Detection_12fps.mp4
 
 echo "Downloading int8 quantized models"
-cd object_detection/yolov5s/FP16-INT8/1; rm *.xml || true; rm *.bin || true;
+cd yolov5s/FP16-INT8/1; rm *.xml || true; rm *.bin || true;
 wget https://github.com/dlstreamer/pipeline-zoo-models/raw/main/storage/yolov5s-416_INT8/FP16-INT8/yolov5s.bin
 wget https://github.com/dlstreamer/pipeline-zoo-models/raw/main/storage/yolov5s-416_INT8/FP16-INT8/yolov5s.xml
 cd ../../..; cd person-detection-retail-0013/FP16-INT8/1; rm *.xml || true; rm *.bin || true; 
@@ -24,4 +24,4 @@ wget https://storage.openvinotoolkit.org/repositories/open_model_zoo/2023.0/mode
 
 echo "Building yolov5 OVMS-CAPI POC"
 cd ../../..
-./object_detection/build-ovms-capi.sh
+./build-ovms-capi.sh
