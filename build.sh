df94a319ea9213f739f608a49d41cc7c47c0051e
gsilva2016 <gabriel.l.silva@intel.com>
2023-08-03 13:16:57 -0700

    bug fixes: oneVPL also working

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 sudo apt update; sudo apt install -y wget git
 
-echo "Download OpenVINO Model Server"
+echo "Download OpenVINO Model Server 2023-gpu"
 docker pull openvino/model_server:2023.0-gpu
 
 echo "Downloading sample video coca-cola-4465029.mp4 https://www.pexels.com/video/4465029/download/"
@@ -14,7 +14,7 @@ wget -O crow-travelers-transport-terminal.mp4 https://www.pexels.com/download/vi
 
 echo "Downloading FP32 and INT8 quantized models"
 # Open source - Yolov5s FP32 version1
-cd object_detection/model_repo/
+cd model_repo/
 cd yolov5s/1; rm *.xml || true; rm *.bin || true;
 # FP32
 #wget https://github.com/dlstreamer/pipeline-zoo-models/raw/main/storage/yolov5s-416/FP32/yolov5s.xml
@@ -49,5 +49,5 @@ wget https://storage.openvinotoolkit.org/repositories/open_model_zoo/2023.0/mode
 wget https://storage.openvinotoolkit.org/repositories/open_model_zoo/2023.0/models_bin/1/face-reidentification-retail-0095/FP16-INT8/face-reidentification-retail-0095.xml
 
 echo "Building yolov5 OVMS-CAPI POC"
-cd ../../../..
+cd ../../..
 ./build-ovms-capi.sh
